a18e1d2543950632300af6c4dd09cd5e729de819
Stan Winitsky <stan.winitsky@gmail.com>
2024-12-16 08:09:10 -0500

    refactor

--- src/test/java/com/example/vuespringbootstarter/VueSpringBootStarterApplicationTests.java
+++ src/test/java/com/example/vuespringbootstarter/VueSpringBootStarterApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.Vuespringbootstarter;
+package com.example.vuespringbootstarter;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
